cb1bebbe39de583fc69eeff00f0c893a4dac6dee
yusanghuck <g01099387636@gmail.com>
2021-02-21 18:01:17 +0900

    fail 9%

--- huck/03_stack_queue/2065_나룻배.py
+++ huck/03_stack_queue/2065_나룻배.py
@@ -1,42 +1,47 @@
 import sys
 import os
-from queue import PriorityQueue
-
-# 1. range
-# - M 나룻배 정원(10000)
-# - t 정박장 편도 시간(10000)
-# - N 손님 수(10000)
-
-# 2. input
-# - 첫째줄에 M, t, N
-# - 다음 N개줄에 도착시간과 정박장위치
-
-# 3. output
-# - 입력받은 순서대로 각 손님이 목적지에 도착하는 시간
-# 손님 {
-#   idx
-#   readyAt
-#   arriveAt
-# }
-
-# 4. spec
-# - 2초
-# - 128MB
+from operator import attrgetter
 
 class Passenger:
-  def __init__(self, idx, readyAt, readyFrom, arriveAt = None):
+  def __init__(self, idx, readyAt, arriveAt = None):
     self.idx = idx
     self.readyAt = readyAt
-    self.readyFrom = readyFrom
-    # self.to = None
     self.arriveAt = arriveAt
-  def __lt__(self, other):
-    if self.arriveAt is None:
-      return self.readyAt < other.readyAt
-    else:
-      return self.idx < other.idx
   def __repr__(self):
-    return 'idx: {}, readAt: {}, arriveAt: {}'.format(self.idx, self.readyAt, self.arriveAt)
+    return 'idx: {}, readyAt: {}, arriveAt: {}'.format(self.idx, self.readyAt, self.arriveAt)
+
+class Queue:
+  def __init__(self):
+    self.__data = []
+
+  def size(self):
+    return len(self.__data)
+
+  def isEmpty(self):
+    return False if self.size() != 0 else True
+
+  def front(self):
+    if (self.isEmpty()):
+      return None
+    return self.__data[0]
+
+  def rear(self):
+    if (self.isEmpty()):
+      return None
+    return self.__data[-1]
+
+  def enqueue(self, data):
+    self.__data.append(data)
+
+  def dequeue(self):
+    if (self.isEmpty()):
+      return None
+    val = self.__data[0]
+    del self.__data[0]
+    return val
+
+  def sort(self, attrSortBy, isDescending = False):
+    self.__data = sorted(self.__data, key=attrgetter(attrSortBy), reverse=isDescending)
 
 def handleInput():
   inputFileSuffix = '_input.txt'
@@ -48,64 +53,73 @@ if __name__ == "__main__":
   if None != os.getenv('IS_LOCAL'):
     handleInput()
 
-  LEFT = 'left'
-  RIGHT = 'right'
-
-  passengers = PriorityQueue(maxsize=10000)
-  onBoarding = PriorityQueue(maxsize=10000)
-  done = PriorityQueue(maxsize=10000)
+  lPassengers = Queue()
+  rPassengers = Queue()
+  onBoarding = Queue()
+  done = Queue()
 
   M, t, N = map(int, input().split())
 
   lines = sys.stdin.readlines()
   for idx, line in enumerate(lines):
-    readyAt, position = line.rstrip('\n').split()
-    passengers.put(Passenger(idx, int(readyAt), position))
+    readyAt, readyFrom = line.rstrip('\n').split()
+    if (readyFrom == 'left'):
+      lPassengers.enqueue(Passenger(idx, int(readyAt)))
+    else:
+      rPassengers.enqueue(Passenger(idx, int(readyAt)))
 
-  # - M 나룻배 정원(10000)
-  # - t 정박장 편도 시간(10000)
-  # 태우고 반대쪽으로
+  lPassengers.sort('readyAt')
+  rPassengers.sort('readyAt')
+
   timestamp = 0
-  currentPos = LEFT
-  while (not passengers.empty()):
-    # 그 정박장으로 가고자 하는 사람들을 우선 모두 내려준다
-    while (not onBoarding.empty()):
-      p = onBoarding.get()
-      p.arriveAt = timestamp
-      done.put(p)
+  isLeft = True
+  boardingCount = 0
+  all = { isLeft: lPassengers, not isLeft: rPassengers }
+  curPassengers = all[isLeft]
+  # 대기하는사람이 없을때까지 반복
+  while (not (lPassengers.isEmpty() and rPassengers.isEmpty())):
+    # 정박장에 도착하면
 
-    boardingCount = 0
-    while(not passengers.empty()):
-      p = passengers.get()
-      # 기다리는 사람이 있다면, (from == currentPos) && (readyAt <= timestamp)
-      if (p.readyFrom == currentPos and p.readyAt <= timestamp):
-        # 그 정박장에서 기다리고 있던 손님들을 배에 태울 수 있는 데까지 태운다
-        if (boardingCount < M):
-          onBoarding.put(p)
-          boardingCount += 1
-        # 손님을 다 태운 후에는 반대쪽 정박장
-        else:
-          passengers.put(p)
-          timestamp += t
-          currentPos = LEFT if currentPos == RIGHT else RIGHT
-          break
-      # 없다면
+    # 1. 그 정박장으로 가고자 하는 사람들을 우선 모두 내려준다
+    if (not onBoarding.isEmpty()):
+      boardingCount = 0
+      while (not onBoarding.isEmpty()):
+        p = onBoarding.dequeue()
+        p.arriveAt = timestamp
+        done.enqueue(p)
+
+    # 2. 승선 가능한 사람 있다면
+    if (not curPassengers.isEmpty()):
+      # 가능한 다 태워
+      while (curPassengers.front() and curPassengers.front().readyAt <= timestamp and boardingCount < M):
+        onBoarding.enqueue(curPassengers.dequeue())
+        boardingCount += 1
+
+    # 3-1. 한명이라도 탔을때
+    if (boardingCount > 0):
+      # 반대쪽으로
+      isLeft = not isLeft
+      timestamp += t
+      curPassengers = all[isLeft]
+    # 3-2. 아무도 안탔을때
+    else:
+      asidePassengers = all[not isLeft]
+      # 반대쪽에 있으면 이동
+      if (not asidePassengers.isEmpty() and asidePassengers.front().readyAt <= timestamp):
+        isLeft = not isLeft
+        timestamp += t
+        curPassengers = all[isLeft]
+      # 반대쪽에 없으면 대기
       else:
-        # 반대쪽 정박장 사람이 있다면
-        if (p.readyAt <= timestamp):
-          # 반대쪽으로이동
-          passengers.put(p)
-          timestamp += t
-          currentPos = LEFT if currentPos == RIGHT else RIGHT
-          break
-        # 반대쪽 정박장 사람이 없다면
-        # elif (p.readyFrom == currentPos):
-        else:
-          # 대기
-          passengers.put(p)
-          timestamp += t
+        timestamp += t
 
-  while (not done.empty()):
-    p = done.get()
+  while (not onBoarding.isEmpty()):
+    p = onBoarding.dequeue()
+    p.arriveAt = timestamp
+    done.enqueue(p)
+
+  done.sort('idx')
+  while (not done.isEmpty()):
+    p = done.dequeue()
     print(p.arriveAt)
 
