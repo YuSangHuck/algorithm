9acce26c15ddda31bc82c63e16a2775f04fdf086
yusanghuck <g01099387636@gmail.com>
2021-08-28 13:41:13 +0900

    solve 1436

--- huck/05주차/brute_force/1436_영화감독_숌.py
+++ huck/05주차/brute_force/1436_영화감독_숌.py
@@ -6,8 +6,28 @@ def handleInput():
   filename, _ = os.path.splitext(__file__)
   sys.stdin = open(filename + suffix)
 
+def isEndNumber(i):
+  res = False
+  # 1자리씩 검사
+  # 1의자리
+  while i != 0:
+    if i % 1000 == 666:
+      res = True
+      break
+
+    i = i // 10
+  return res
+
 def solution(n):
-  return ((n-1) * 1000) + 666
+  hitCount = 0
+  i = 665
+
+  while hitCount != n:
+    i += 1
+    if isEndNumber(i):
+      hitCount += 1
+
+  return i
 
 if __name__ == '__main__':
   if os.getenv('IS_LOCAL'):
@@ -15,3 +35,7 @@ if __name__ == '__main__':
 
   n = int(sys.stdin.readline())
   print(solution(n))
+
+  # for n in range(1, 100):
+  #   print(solution(n))
+
