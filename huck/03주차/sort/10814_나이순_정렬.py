07bb021f3f68b01f1db08aaa23f7405c890d7c75
yusanghuck <g01099387636@gmail.com>
2021-08-28 16:01:17 +0900

    solve 10814

--- huck/03주차/sort/10814_나이순_정렬.py
+++ huck/03주차/sort/10814_나이순_정렬.py
@@ -9,15 +9,19 @@ def handlerInput():
   sys.stdin = open(fn + suffix, 'r')
 
 def solution():
-  n = int(sys.stdin.readline())
+  _ = int(sys.stdin.readline())
   lines = sys.stdin.readlines()
+
   people = []
-  for idx, line in enumerate(lines):
+  for line in lines:
     age, name = line.rstrip('\n').split()
-    people.append((age, name))
+    people.append((int(age), name))
   # sort는 기본적으로 stable sort이다
   people.sort(key=itemgetter(0))
-  print('\n'.join(map(lambda person: str.format('{0} {1}', person[0], person[1]), people)))
+
+  print('\n'.join(map(lambda person:
+      str.format('{0} {1}', person[0], person[1]),
+    people)))
 
 if __name__ == '__main__':
   if os.getenv('IS_LOCAL'):
