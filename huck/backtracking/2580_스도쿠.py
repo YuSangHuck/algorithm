dfb83f19d53d18364f6281d32a25ad654c47811e
yusanghuck <g01099387636@gmail.com>
2021-09-05 18:46:25 +0900

    solve 2580

--- huck/backtracking/2580_스도쿠.py
+++ huck/backtracking/2580_스도쿠.py
@@ -5,8 +5,8 @@ import sys
 import os
 
 g_board = []
+g_zeros = []
 g_isEnd = False
-g_candidates = [1,2,3,4,5,6,7,8,9]
 
 def setStdin():
   suffix = '_input.txt'
@@ -20,86 +20,54 @@ def printBoard():
 
 
 
-def promising(xPos, yPos):
+def getCandidates(x, y):
   global g_board
 
-  # 가로체크
-  check = [0] * 10
-  for x in range(9):
-    if g_board[yPos][x] != 0 and check[g_board[yPos][x]] == 1:
-      return False
-    else:
-      check[g_board[yPos][x]] = 1
-  # 세로체크
-  check = [0] * 10
-  for y in range(9):
-    if g_board[y][xPos] != 0 and check[g_board[y][xPos]] == 1:
-      return False
-    else:
-      check[g_board[y][xPos]] = 1
-  # subSquare체크
-  check = [0] * 10
-  for y in range(3*(yPos//3), 3 + 3*(yPos//3)):
-    for x in range(3*(xPos//3), 3 + 3*(xPos//3)):
-      if g_board[y][x] != 0 and check[g_board[y][x]] == 1:
-        return False
-      else:
-        check[g_board[y][x]] = 1
+  candidates = [1,2,3,4,5,6,7,8,9]
 
-  return True
+  for idx in range(9):
+    # 가로
+    if g_board[idx][x] in candidates:
+      candidates.remove(g_board[idx][x])
+    # 세로
+    if g_board[y][idx] in candidates:
+      candidates.remove(g_board[y][idx])
+
+  offsetY = y // 3
+  offsetX = x // 3
+  for j in range(3 * offsetY, 3 * (1 + offsetY)):
+    for i in range(3 * offsetX, 3 * (1 + offsetX)):
+      if g_board[j][i] in candidates:
+        candidates.remove(g_board[j][i])
+
+  return candidates
 
 
 # row, col부터 탐색
-def dfs(row, col):
-  global g_board, g_isEnd, g_candidates
-
-  g_isEnd = 0 not in g_board[8]
+def dfs(idx):
+  global g_board, g_zeros, g_isEnd
 
   if g_isEnd:
+    return
+
+  if idx == len(g_zeros):
     printBoard()
-    return g_isEnd
+    g_isEnd = True
+    return
 
-  # if 0 in g_board[row]:
-  #   x = g_board[row].index(0)
-  #   for candidate in g_candidates:
-  #     g_board[row][x] = candidate
-
-  #     if promising(x, row):
-  #       if 0 in g_board[row]:
-  #         dfs(row, x + 1)
-  #       else:
-  #         dfs(row + 1, 0)
-
-  #       if g_isEnd:
-  #         return
-
-  # y는 row로 고정, x만 탐색
-  for x in range(col, 9):
-    # 값이 비어있다면
-    if g_board[row][x] == 0:
-      # 1~9를 넣어놓고
-      for candidate in g_candidates:
-        g_board[row][x] = candidate
-
-        # 유효한지 체크
-        if promising(x, row):
-          # 유효하면 다음 진행
-          if 0 in g_board[row]:
-            dfs(row, x + 1)
-          else:
-            dfs(row + 1, 0)
-
-          if g_isEnd:
-            return
-        else:
-          # 유효하지않으면 값 초기화
-          g_board[row][x] = 0
+  (x, y) = g_zeros[idx]
+  candidates = getCandidates(x,y)
+  for candidate in candidates:
+    g_board[y][x] = candidate
+    dfs(idx + 1)
+    g_board[y][x] = 0
 
 def sol():
-  global g_board
+  global g_board, g_zeros
   g_board = [list(map(int, line.split())) for line in sys.stdin.readlines()]
+  g_zeros = [(x,y) for y in range(9) for x in range(9) if g_board[y][x] == 0]
 
-  dfs(0, 0)
+  dfs(0)
 
 
 
@@ -108,3 +76,17 @@ if __name__ == '__main__':
     setStdin()
 
   sol()
+
+# case0 (idx)
+# elapse: 0.001680612564086914 // all zero
+
+# cas1 (row, col)
+# elapse: 0.00022721290588378906
+# elapse: 0.016598939895629883 // all zero
+
+# case2 (col)
+# elapse: 0.0002460479736328125
+# elapse: 0.41098690032958984
+
+# case3 ()
+# elapse: 0.0003452301025390625
