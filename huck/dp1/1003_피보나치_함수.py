72bc383cd18cfc5f6b1e43033c66c1e8625d2e33
yusanghuck <g01099387636@gmail.com>
2021-09-13 15:36:59 +0900

    fail 1003, why?

--- huck/dp1/1003_피보나치_함수.py
+++ huck/dp1/1003_피보나치_함수.py
@@ -1,10 +1,11 @@
+# FIXME 실제 fibo의 값은 필요없어서 제거했는데 왜 틀리지?
 import sys
 import os
 
 
-g_dp = [-1] * 41 # max(n) == 40
-g_dpZero = [-1] * 41 # max(n) == 40
-g_dpOne = [-1] * 41 # max(n) == 40
+g_dp = [[-1] * 41, [-1] * 41] # max(n) == 40
+ZERO = 0
+ONE = 1
 
 def setStdin():
   suffix = '_input.txt'
@@ -14,28 +15,25 @@ def setStdin():
 def fibo(n):
   global g_dp
 
-  if g_dp[n-1] != -1 and g_dp[n-2] != -1:
-    g_dp[n] = g_dp[n-1] + g_dp[n-2]
-    g_dpZero[n] = g_dpZero[n-1] + g_dpZero[n-2]
-    g_dpOne[n] = g_dpOne[n-1] + g_dpOne[n-2]
-    return g_dp[n]
+  if g_dp[ZERO][n-1] != -1 and g_dp[ZERO][n-2] != -1 and g_dp[ONE][n-1] != -1 and g_dp[ONE][n-2] != -1:
+    g_dp[ZERO][n] = g_dp[ZERO][n-1] + g_dp[ZERO][n-2]
+    g_dp[ONE][n] = g_dp[ONE][n-1] + g_dp[ONE][n-2]
+    return
 
   if n == 0:
-    g_dp[0] = 0
-    g_dpZero[0] = 1
-    g_dpOne[0] = 0
-    return 0
+    g_dp[ZERO][0] = 1
+    g_dp[ONE][0] = 0
+    return
   elif n == 1:
-    g_dp[1] = 1
-    g_dpZero[1] = 0
-    g_dpOne[1] = 1
-    return 1
+    g_dp[ZERO][1] = 0
+    g_dp[ONE][1] = 1
+    return
   else:
-    g_dp[n-1] = fibo(n-1)
-    g_dp[n-2] = fibo(n-2)
-    g_dpZero[n] = g_dpZero[n-1] + g_dpZero[n-2]
-    g_dpOne[n] = g_dpOne[n-1] + g_dpOne[n-2]
-    return g_dp[n-1] + g_dp[n-2]
+    fibo(n-1)
+    fibo(n-2)
+    g_dp[ZERO][n] = g_dp[ZERO][n-1] + g_dp[ZERO][n-2]
+    g_dp[ONE][n] = g_dp[ONE][n-1] + g_dp[ONE][n-2]
+    return
 
 def sol():
   t = int(sys.stdin.readline())
@@ -43,7 +41,7 @@ def sol():
   for _ in range(t):
     n = int(sys.stdin.readline())
     fibo(n)
-    print(f'{g_dpZero[n]} {g_dpOne[n]}')
+    print(f'{g_dp[ZERO][n]} {g_dp[ONE][n]}')
 
 if __name__ == '__main__':
   if os.getenv('IS_LOCAL'):
