8258d5e9097cd0ed187ee61c06f0b11bf3c2cd7e
yusanghuck <g01099387636@gmail.com>
2021-10-25 16:13:35 +0900

    wrong 2493

--- 2493_탑.go
+++ 2493_탑.go
@@ -28,27 +28,20 @@ func sol() {
 
 	line, _ := r.ReadString('\n')
 	s_tops := strings.Fields(line) // 0부터 시작
-	n_tops := [500001]int{}        // 1부터 시작
-	var res [500001]string
+	n_tops := [500000]int{}        // 0부터 시작
+	var res [500000]string
 	for i := 0; i < n; i++ {
-		n_tops[1+i], _ = strconv.Atoi(s_tops[i])
-	}
+		n_tops[i], _ = strconv.Atoi(s_tops[i])
 
-	// 오른쪽부터
-	for i := n; i > 0; i-- {
+		res[i] = "0"
 		for j := i - 1; j > 0; j-- {
 			// 수신하면
 			if n_tops[j] >= n_tops[i] {
-				res[i-1] = strconv.Itoa(j)
+				res[i] = strconv.Itoa(1 + j)
 				break
-			}
-			if j == 1 {
-				res[i-1] = "0"
 			}
 		}
 	}
-	// 제일 왼쪽거 1개
-	res[0] = "0"
 
 	fmt.Fprint(w, strings.Join(res[:n], " "))
 
