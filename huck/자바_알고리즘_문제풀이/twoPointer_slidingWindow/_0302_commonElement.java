33e1cebd494d3e2ca433b8237ff482edb8f05a83
yusanghuck <g01099387636@gmail.com>
2022-06-25 03:06:53 +0900

    2. 공통원소 구하기

---
+++ huck/자바_알고리즘_문제풀이/twoPointer_slidingWindow/_0302_commonElement.java
@@ -0,0 +1,58 @@
+//https://cote.inflearn.com/contest/10/problem/03-02
+package huck.자바_알고리즘_문제풀이.twoPointer_slidingWindow;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+//public class Main {
+public class _0302_commonElement {
+
+    public String solution(int n, int m, int[] arr1, int[] arr2) {
+//        arr1, arr2는 정렬된 상태
+        StringBuilder sb = new StringBuilder();
+
+        int p1 = 0;
+        int p2 = 0;
+
+        while (p1 < n && p2 < m) {
+            if (arr1[p1] > arr2[p2]) {
+                p2++;
+            } else if (arr1[p1] < arr2[p2]) {
+                p1++;
+            } else {
+                sb.append(arr1[p1]);
+                sb.append(" ");
+                p1++;
+            }
+
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        Main T = new Main();
+        _0302_commonElement T = new _0302_commonElement();
+
+        FileInputStream fileInputStream = new FileInputStream("./huck/자바_알고리즘_문제풀이/twoPointer_slidingWindow/_0302_commonElement_input.txt");
+
+//        Scanner kb = new Scanner(System.in);
+        Scanner kb = new Scanner(fileInputStream);
+        int n = kb.nextInt();
+        int[] arr1 = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr1[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] arr2 = new int[m];
+        for (int i = 0; i < m; i++) {
+            arr2[i] = kb.nextInt();
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        System.out.println(T.solution(n, m, arr1, arr2));
+    }
+}
