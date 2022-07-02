47b792cda1762e72a47dbd73620025e40c9618e7
yusanghuck <g01099387636@gmail.com>
2022-07-02 23:39:39 +0900

    _0704_재귀함수_피보나치

---
+++ huck/자바_알고리즘_문제풀이/recursive_tree_graph/_0704_재귀함수_피보나치.java
@@ -0,0 +1,32 @@
+package huck.자바_알고리즘_문제풀이.recursive_tree_graph;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+//public class Main {
+public class _0704_재귀함수_피보나치 {
+    private int DFS(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        } else {
+            return DFS(n - 1) + DFS(n - 2);
+        }
+    }
+
+    private int solution(int i) {
+        return DFS(i);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        _0704_재귀함수_피보나치 T = new _0704_재귀함수_피보나치();
+        FileInputStream fileInputStream = new FileInputStream("huck/자바_알고리즘_문제풀이/recursive_tree_graph/_0704_재귀함수_피보나치.txt");
+        Scanner kb = new Scanner(fileInputStream);
+//        Main T = new Main();
+//        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.println("i = " + i + ", v = " + T.solution(i));
+        }
+    }
+}
