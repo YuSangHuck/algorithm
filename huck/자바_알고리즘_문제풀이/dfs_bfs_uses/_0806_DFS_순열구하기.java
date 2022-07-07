4ca09ac40cf4ccf68379b6be52300bfc4bdb1d19
yusanghuck <g01099387636@gmail.com>
2022-07-07 16:25:37 +0900

    _0806_DFS_순열구하기

---
+++ huck/자바_알고리즘_문제풀이/dfs_bfs_uses/_0806_DFS_순열구하기.java
@@ -0,0 +1,53 @@
+//https://cote.inflearn.com/contest/10/problem/07-08
+package huck.자바_알고리즘_문제풀이.dfs_bfs_uses;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+//public class Main {
+public class _0806_DFS_순열구하기 {
+    static int m, n;
+    static int[] arr, ch;
+
+    private void DFS(int depth) {
+        if (depth == m) {
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 1) {
+                    System.out.print(arr[i] + " ");
+                }
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (ch[i] == 0) {
+                ch[i] = 1;
+                DFS(1 + depth);
+                ch[i] = 0;
+            }
+        }
+    }
+
+    private void solution() {
+        DFS(0);
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        _0806_DFS_순열구하기 T = new _0806_DFS_순열구하기();
+        FileInputStream fileInputStream = new FileInputStream("huck/자바_알고리즘_문제풀이/dfs_bfs_uses/_0806_DFS_순열구하기.txt");
+        Scanner kb = new Scanner(fileInputStream);
+//        Main T = new Main();
+//        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        arr = new int[n]; // 오름차순 정렬
+        ch = new int[n]; // 오름차순 정렬
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        T.solution();
+    }
+
+}
