a6458e88946d237817c501875d53cbe866ce8ce8
yusanghuck <g01099387636@gmail.com>
2022-07-08 22:15:43 +0900

    _0902_회의실배정(greedy. 정렬 잘)

--- huck/자바_알고리즘_문제풀이/greedy/_0902_회의실배정.java
+++ huck/자바_알고리즘_문제풀이/greedy/_0902_회의실배정.java
@@ -10,9 +10,16 @@ import java.util.Scanner;
 //public class Main {
 public class _0902_회의실배정 {
     static class Meet implements Comparable<Meet> {
+        // 최선은 먼저시작하는게 아닌 회의끝나는시각이 빨리끝나는거부터. 회의가 끝나야 시작할수있으므로
+        // 회의시간이 짧은거부터가 아니라?
+        // 둘다해야함. 1순위 끝나는시각으로 오름차순. 2순위 시작시각으로 오름차순
         @Override
         public int compareTo(Meet o) {
-            return this.s - o.s;
+            if (this.e == o.e) {
+                return this.s - o.s;
+            } else {
+                return this.e - o.e;
+            }
         }
 
         int s, e;
@@ -32,24 +39,16 @@ public class _0902_회의실배정 {
     }
 
     private void solution(int n, ArrayList<Meet> arr) {
-        int count = Integer.MIN_VALUE;
+        int count = 0;
         Collections.sort(arr);
-        for (int i = 0; i < arr.size(); i++) {
-//            1개 고정
-            Meet first = arr.get(i);
-//            차례대로 비교
-            int tmp = 1;
-            for (int j = i + 1; j < n; j++) {
-                Meet second = arr.get(j);
-                if (first.e <= second.s) {
-                    tmp++;
-                    first = second;
-                }
-
+        int e = 0;
+        for (Meet meet : arr) {
+            if (meet.s >= e) {
+                count++;
+                e = meet.e;
             }
-            count = Math.max(count, tmp);
-
         }
+
         System.out.println(count);
     }
 
