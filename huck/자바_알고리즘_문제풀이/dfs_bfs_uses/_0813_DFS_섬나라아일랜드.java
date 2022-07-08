acd56db285a55c13b760d211cf20fb30d1f9e819
yusanghuck <g01099387636@gmail.com>
2022-07-08 16:40:47 +0900

    _0813_DFS_섬나라아일랜드

--- huck/자바_알고리즘_문제풀이/dfs_bfs_uses/_0813_DFS_섬나라아일랜드.java
+++ huck/자바_알고리즘_문제풀이/dfs_bfs_uses/_0813_DFS_섬나라아일랜드.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 //public class Main {
 public class _0813_DFS_섬나라아일랜드 {
     private static int n;
-    private static int[] dx={0,1,1,1,-1,-1,-1}, dy = {1, 1, 0, -1, -1, -1, 0, 1};
+    private static final int[] dx = {0, 1, 1, 1, 0, -1, -1, -1}, dy = {1, 1, 0, -1, -1, -1, 0, 1};
     private static int[][] board;
 
     private void DFS(int x, int y) {
