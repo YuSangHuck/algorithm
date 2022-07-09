5220bad57d44fd4e4bc0295ef6bab55280a10d1b
yusanghuck <g01099387636@gmail.com>
2022-07-09 15:29:39 +0900

    _0906_친구인가(fail, wrong)

--- huck/자바_알고리즘_문제풀이/greedy/_0906_친구인가.java
+++ huck/자바_알고리즘_문제풀이/greedy/_0906_친구인가.java
@@ -1,4 +1,4 @@
-//https://cote.inflearn.com/contest/10/problem/09-05
+//https://cote.inflearn.com/contest/10/problem/09-06
 package huck.자바_알고리즘_문제풀이.greedy;
 
 import java.io.FileInputStream;
@@ -7,6 +7,43 @@ import java.util.*;
 
 //public class Main {
 public class _0906_친구인가 {
+    private int[] unf;
+
+    private int find(int n) {
+        if (unf[n] == n) {
+            return n;
+        } else {
+            return unf[n] = find(unf[n]);
+        }
+    }
+
+    private void union(int a, int b) {
+        int fa = find(a);
+        int fb = find(b);
+        if (fa != fb) {
+            unf[fa] = fb;
+        }
+    }
+
+    private void soultion(int n, int m, int[][] arr, int[][] check) {
+        unf = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            unf[i] = i;
+        }
+        for (int i = 0; i < m; i++) {
+            union(arr[i][0], arr[i][1]);
+        }
+
+        int a = check[0][0];
+        int b = check[0][0];
+
+        if (find(a) != find(b)) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
+        }
+
+    }
 
     public static void main(String[] args) throws FileNotFoundException {
 //        Main T = new Main();
@@ -15,63 +52,17 @@ public class _0906_친구인가 {
         FileInputStream fileInputStream = new FileInputStream("./huck/자바_알고리즘_문제풀이/greedy/_0906_친구인가.txt");
         Scanner kb = new Scanner(fileInputStream);
 
-//        상호참조같은 느낌?
-//        친구쌍 (a,b), (a,c), (d,e)가 주어졌을때 벤다이어그램으로 생각하면 편함
-
         int n = kb.nextInt();
         int m = kb.nextInt();
-        ArrayList<HashSet<Integer>> sets = new ArrayList<>();
-        int setsCount = 0;
-
-        int a = kb.nextInt();
-        int b = kb.nextInt();
-        HashSet<Integer> hs = new HashSet<>();
-        hs.add(a);
-        hs.add(b);
-        setsCount++;
-        sets.add(new HashSet<>(hs));
-
-        for (int i = 1; i < m; i++) { // 친구쌍
-            a = kb.nextInt();
-            b = kb.nextInt();
-            boolean newSet = true;
-
-            for (int j = 0; j < setsCount; j++) { // sets 순회
-                HashSet<Integer> set = sets.get(j);
-                boolean checkA = set.contains(a);
-                boolean checkB = set.contains(b);
-                if (checkA || checkB) {
-                    set.add(a);
-                    set.add(b);
-                    newSet = false;
-                }
-            }
-            if (newSet) {
-                hs = new HashSet<>();
-                hs.add(a);
-                hs.add(b);
-                setsCount++;
-                sets.add(new HashSet<>(hs));
-            }
+        int[][] arr = new int[m][2];
+        for (int i = 0; i < m; i++) {
+            arr[i][0] = kb.nextInt();
+            arr[i][1] = kb.nextInt();
         }
+        int[][] check = new int[1][2];
+        check[0][0] = kb.nextInt();
+        check[0][1] = kb.nextInt();
 
-        int i = kb.nextInt();
-        int j = kb.nextInt();
-        boolean flag = true;
-        for (HashSet<Integer> set : sets) {
-            boolean checkI = set.contains(i);
-            boolean checkJ = set.contains(j);
-            if (checkI && checkJ) {
-                System.out.println("YES");
-                flag = false;
-            }
-        }
-        if (flag) {
-            System.out.println("NO");
-        }
-
-//        for (HashSet<Integer> set : sets) {
-//            System.out.println("set = " + set);
-//        }
+        T.soultion(n, m, arr, check);
     }
 }
