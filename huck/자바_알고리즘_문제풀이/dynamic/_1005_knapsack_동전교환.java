504356f9c2a5138bfd8d6b5a025a5e21b8e56f27
yusanghuck <g01099387636@gmail.com>
2022-07-10 21:28:47 +0900

    _1005_knapsack_동전교환

--- huck/자바_알고리즘_문제풀이/dynamic/_1005_knapsack_동전교환.java
+++ huck/자바_알고리즘_문제풀이/dynamic/_1005_knapsack_동전교환.java
@@ -10,21 +10,17 @@ import java.util.Scanner;
 public class _1005_knapsack_동전교환 {
     private void solution(int n, int[] arr, int m) {
         Arrays.sort(arr);
-
-        int answer = 0;
-        int change = m;
-        for (int i = n - 1; i >= 0; i--) {
-            if (change >= arr[i]) {
-                answer += change / arr[i];
-                change = change % arr[i];
-                if (change == 0) {
-                    break;
-                }
+//        dy[i]는 i금액을 만드는데 필요한 동전수의 최소값
+        int[] dy = new int[m + 1];
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = arr[i]; j <= m; j++) {
+                dy[j] = Math.min(dy[j], dy[j - arr[i]] + 1);
             }
         }
 
-        System.out.println(answer);
-
+        System.out.println(dy[m]);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
