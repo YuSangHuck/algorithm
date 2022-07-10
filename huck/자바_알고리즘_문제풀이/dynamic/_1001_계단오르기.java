65aa53a0ac7b212e995db430da5767979b02ae69
yusanghuck <g01099387636@gmail.com>
2022-07-10 17:54:27 +0900

    _1001_계단오르기(강의스타일로 정정)

--- huck/자바_알고리즘_문제풀이/dynamic/_1001_계단오르기.java
+++ huck/자바_알고리즘_문제풀이/dynamic/_1001_계단오르기.java
@@ -8,17 +8,13 @@ import java.util.Scanner;
 //public class Main {
 public class _1001_계단오르기 {
     private void solution(int n) {
-//        한번에 1계단 or 2계단
-//        3번째계단을 올라오는건 2 -> 3 or 1 -> 3밖에 없다
-//        n번째계단을 올라오는건 n-1 -> n or n-2 -> n밖에 없다
-//        arr[n] = arr[n-1] + arr[n-2]
-        int[] arr = new int[n + 1];
-        arr[0] = 1;
-        arr[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            arr[i] = arr[i - 2] + arr[i - 1];
+        int[] dy = new int[n + 1];
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dy[i] = dy[i - 2] + dy[i - 1];
         }
-        System.out.println(arr[n]);
+        System.out.println(dy[n]);
     }
 
     public static void main(String[] args) throws FileNotFoundException {
