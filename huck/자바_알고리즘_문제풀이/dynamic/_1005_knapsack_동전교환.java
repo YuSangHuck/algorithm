c24969b6727dff8ded8ceaee7c5ad4fbe65ba047
yusanghuck <g01099387636@gmail.com>
2022-07-10 21:08:07 +0900

    _1005_knapsack_동전교환(fail,TLE)

---
+++ huck/자바_알고리즘_문제풀이/dynamic/_1005_knapsack_동전교환.java
@@ -0,0 +1,51 @@
+//https://cote.inflearn.com/contest/10/problem/10-05
+package huck.자바_알고리즘_문제풀이.dynamic;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+//public class Main {
+public class _1005_knapsack_동전교환 {
+    private void solution(int n, int[] arr, int m) {
+        System.out.println("n = " + n + ", arr = " + Arrays.toString(arr) + ", m = " + m);
+        Arrays.sort(arr);
+
+        int answer = 0;
+        int change = m;
+        for (int i = n - 1; i >= 0; i--) {
+            if (change == 0) {
+                break;
+            }
+            while (change != 0) {
+                if (change >= arr[i]) {
+                    change -= arr[i];
+                    answer++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        Main T = new Main();
+//        Scanner kb = new Scanner(System.in);
+        _1005_knapsack_동전교환 T = new _1005_knapsack_동전교환();
+        FileInputStream fileInputStream = new FileInputStream("huck/자바_알고리즘_문제풀이/dynamic/_1005_knapsack_동전교환.txt");
+        Scanner kb = new Scanner(fileInputStream);
+
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        T.solution(n, arr, m);
+
+    }
+}
