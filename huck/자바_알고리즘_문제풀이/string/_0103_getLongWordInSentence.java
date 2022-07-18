32e65454ad47d517150cd0c14755a69fdd6be1c9
yusanghuck <g01099387636@gmail.com>
2022-07-18 15:27:38 +0900

    splitIter로 직접 max 계산하거나 splitListOfCustomStringSort으로 정렬하거나
    Collections.sort, List.sort 전부 내부적으로 Arrays.sort 호출하는데
    
    어떤경우는 360ms, 어떤경우는 160ms 나오는지 모르겠음
    
    Comparable을 직접 구현한 경우만 160이 나옴
    
    Comparator를 변수로 선언해놓고 sort의 인자로 넘겨도 360 나옴

--- huck/자바_알고리즘_문제풀이/string/_0103_getLongWordInSentence.java
+++ huck/자바_알고리즘_문제풀이/string/_0103_getLongWordInSentence.java
@@ -3,62 +3,92 @@ package huck.자바_알고리즘_문제풀이.string;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
+//public class Main {
 public class _0103_getLongWordInSentence {
 
-
-    public String getLongWordInSentence1(String sentence) {
-        String ret = "";
-        int min = Integer.MIN_VALUE;
-        String[] sList = sentence.split(" ");
-        for (String s : sList) {
-            int len = s.length();
-            if (len > min) {
-                min = len;
-                ret = s;
-            }
-        }
-        return ret;
-    }
-
-    private String getLongWordInSentence2(String sentence) {
-        String ret = "";
-        int min = Integer.MIN_VALUE, pos;
-
-        while ((pos = sentence.indexOf(' ')) != -1) {
-            String substring = sentence.substring(0, pos);
-            int length = substring.length();
-            if (length > min) {
-                min = length;
-                ret = substring;
-            }
-            sentence = sentence.substring(1 + pos);
-        }
-        if (sentence.length() > min) {
-            ret = sentence;
-        }
-
-        return ret;
-    }
-
-    public String solution(String str) {
+    private static String splitIter(String str) { // 153ms
         String answer = "";
 
-        answer = getLongWordInSentence1(str);
-        answer = getLongWordInSentence2(str);
+        String[] arr = str.split(" ");
 
+        int max = Integer.MIN_VALUE;
+        for (String s : arr) {
+            if (s.length() > max) {
+                answer = s;
+                max = s.length();
+            }
+        }
 
         return answer;
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        _0103_getLongWordInSentence T = new _0103_getLongWordInSentence();
-        FileInputStream fileInputStream = new FileInputStream("./input.txt");
+    private static String splitArraysSort(String str) { // 350ms
+        String[] arr = str.split(" ");
+        Comparator<String> stringComparator = (o1, o2) -> o2.length() - o1.length();
+        Arrays.sort(arr, stringComparator); // n
 
+        return arr[0]; // 2n
+    }
+
+    private static String splitListSort(String str) {
+        Comparator<String> stringComparator = (o1, o2) -> o2.length() - o1.length();
+
+        List<String> list = Arrays.asList(str.split(" ")); // 356ms
+
+//        List<String> list = new ArrayList<>(); // 352ms
+//        Collections.addAll(list, str.split(" "));
+
+//        List<String> list = new ArrayList<>(); // 357ms
+//        for (String s : str.split(" ")) {
+//            list.add(s);
+//        }
+
+        list.sort(stringComparator);
+        return list.get(0);
+
+    }
+
+    static class CustomString implements Comparable<CustomString> {
+
+        String s;
+
+        public CustomString(String s) {
+            this.s = s;
+        }
+
+        @Override
+        public int compareTo(CustomString o) {
+            return o.s.length() - s.length();
+        }
+    }
+    private static String splitListOfCustomStringSort(String str) {
+        ArrayList<CustomString> arr = new ArrayList<>();
+        for (String s : str.split(" ")) {
+            arr.add(new CustomString(s));
+        }
+
+        arr.sort(null); // 162ms
+        Collections.sort(arr); // 160ms
+
+        return arr.get(0).s;
+    }
+
+    public static String solution(String str) {
+//        return splitIter(str); // 153ms
+//        return splitArraysSort(str); // 355ms
+//        return splitListSort(str); // 360ms
+        return splitListOfCustomStringSort(str); // 160ms
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("./huck/자바_알고리즘_문제풀이/string/_0103_getLongWordInSentence.txt");
         Scanner kb = new Scanner(fileInputStream);
+//        Scanner kb = new Scanner(System.in);
+
         String str = kb.nextLine();
 
-        System.out.println(T.solution(str));
+        System.out.println(solution(str));
     }
 }
