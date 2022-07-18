04f462dfb47f8d3d37ab84bca7b3f91fadc39e91
yusanghuck <g01099387636@gmail.com>
2022-07-18 19:04:28 +0900

    문자열 뒤집는건 StritBuilder.reverse

--- huck/자바_알고리즘_문제풀이/string/_0104_reverseString.java
+++ huck/자바_알고리즘_문제풀이/string/_0104_reverseString.java
@@ -3,52 +3,33 @@ package huck.자바_알고리즘_문제풀이.string;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
+//public class Main {
 public class _0104_reverseString {
-    public String reverseString1(String str) {
-        return new StringBuilder(str).reverse().toString();
-    }
 
-    public String reverseString2(String str) {
-        char[] cList = str.toCharArray();
-        int lt = 0, rt = str.length() - 1;
-        while (lt < rt) {
-            char tmp = cList[lt];
-            cList[lt] = cList[rt];
-            cList[rt] = tmp;
-            lt++;
-            rt--;
-        }
-        return String.valueOf(cList);
-    }
-
-    public ArrayList<String> solution(String[] strList) {
-        ArrayList<String> answer = new ArrayList<>();
+    public static String solution(int n, String[] strList) {
+        StringBuilder answer = new StringBuilder();
 
         for (String s : strList) {
-//            answer.add(reverseString1(s));
-            answer.add(reverseString2(s));
+            answer.append(new StringBuilder(s).reverse()).append('\n');
         }
 
-        return answer;
+        return answer.toString();
     }
 
     public static void main(String[] args) throws FileNotFoundException {
-        _0104_reverseString T = new _0104_reverseString();
-        FileInputStream fileInputStream = new FileInputStream("./input.txt");
-
+        FileInputStream fileInputStream = new FileInputStream("./huck/자바_알고리즘_문제풀이/string/_0104_reverseString.txt");
         Scanner kb = new Scanner(fileInputStream);
+//        Main T = new Main();
+//        Scanner kb = new Scanner(System.in);
+
         int n = kb.nextInt();
         String[] strList = new String[n];
         for (int i = 0; i < n; i++) {
             strList[i] = kb.next();
-//            strList[i] = kb.nextLine();
         }
 
-        for (String s : T.solution(strList)) {
-            System.out.println(s);
-        }
+        System.out.println(solution(n, strList));
     }
 }
