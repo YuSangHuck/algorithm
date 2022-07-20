2b32fb3f40148d5f0934142313eef8f47f18e18c
yusanghuck <g01099387636@gmail.com>
2022-07-21 02:42:37 +0900

    baekjoon._2098_외판원순회 fail(wrong)

--- baekjoon/_2098_외판원순회.java
+++ baekjoon/_2098_외판원순회.java
@@ -19,36 +19,57 @@ import java.util.Scanner;
 //              findPath(start, leftCity, visited, sum + D[last][leftCity]
 //              visted에 leftCity 제거
 
+//  dp
+//  1. 중복케이스를 찾는다
+//  -> 같은 인자에 대한 중복호출
+//  -> ex)  dfs(2, {1,3,2}) -> dfs(4, {1,3,2,4})
+//          dfs(3, {1,2,3}) -> dfs(4, {1,2,3,4})
+//          last가 4이고, visited는 실제로 비트이므로 동일하다
+//  -> 중복을 줄이기위한 dp의 캐시 크기만 정하면 됨
+//      세개의 변수(start,last,V)
+//      start,last는 n을 따라가고
+//      V는 n개의 도시를 각각 0,1로 표현하므로 2^n
+//      따라서 캐시 크기는 n^2*2^n
+//  -> dp에서 시간복잡도는 캐시의 크기를 따라간다
+//      따라서 시간복잡도는  O(n^2*2^n)
+//  2. 한번 방문한 도시는 재방문하지 않는다
+//      재방문하지 않고, 시작도시로 돌아온다
+//      사이클
+//      사이클이므로 시작지점은 무관하다
+//      n이 하나 줄어 n*2^n
+
 //public class Main {
 public class _2098_외판원순회 {
 
     private static int n, start, ALL_VISITED, answer = Integer.MAX_VALUE;
-    private static int[][] board;
+    private static int[][] board, dp;
 
-    private static void dfs(int last, int visited, int sum) {
+    private static int dfs(int last, int visited) {
         if (visited == ALL_VISITED) {
-            int costToStart = board[last][start];
-            if (costToStart != 0) {
-                answer = Math.min(answer, costToStart + sum);
-            }
-            return;
+            return board[last][start] != 0 ? board[last][start] : Integer.MAX_VALUE;
         }
+
+        if (dp[last][visited] != 0) {
+            return dp[last][visited];
+        }
+
+        int tmp = Integer.MAX_VALUE;
         for (int left = 0; left < n; left++) {
             boolean notVisit = ((visited >> left) & 1) == 0;
             if (notVisit && board[last][left] != 0) {
-                dfs(left, visited | (1 << left), sum + board[last][left]);
+                tmp = Math.min(tmp,
+                        dfs(left, visited | (1 << left)) + board[last][left]);
             }
         }
+        dp[last][visited] = tmp;
+        return tmp;
     }
 
     private static int solution() {
         ALL_VISITED = (1 << n) - 1;
-        for (int i = 0; i < n; i++) {
-            start = i;
-            dfs(i, 1 << i, 0);
-        }
-
-        return answer;
+        start = 0;
+        dfs(start, 1 << start);
+        return dp[start][1<<start];
     }
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -58,6 +79,7 @@ public class _2098_외판원순회 {
 
         n = kb.nextInt();
         board = new int[n][n];
+        dp = new int[n][1 << n]; // [n]: last의 경우의수 n개, [1<<n]: visited의 상태값 경우의수 2^n
         for (int y = 0; y < n; y++) {
             for (int x = 0; x < n; x++) {
                 board[y][x] = kb.nextInt(); // 비용행렬
